a1d8c0de126a0498063cf0535ff2c55c7dd8f32e
manjitkarve <manjit.karve@gmail.com>
2015-07-05 04:05:58 +0530

    Sass rewritten

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	if (window.location.pathname==="/"){
 		$("body").attr("pagename", "index");
 	} else {
-		$("body").attr("pagename", window.location.pathname.substr(0, window.location.pathname.indexOf(".shtml")).substr(1));	
+		$("body").attr("pagename", window.location.pathname.substr(0,    window.location.pathname.indexOf(".shtml")).substr(1));
 	}
 	
 	$("#pageContainer>main").perfectScrollbar();
